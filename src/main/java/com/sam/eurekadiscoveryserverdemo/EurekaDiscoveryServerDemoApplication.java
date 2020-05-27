4da86d6f2e844fa1d61e17ec5a203b1db1e41e17
Samim <samimaftabahmed@gmail.com>
2020-05-27 23:16:34 +0530

    first commit

---
+++ src/main/java/com/sam/eurekadiscoveryserverdemo/EurekaDiscoveryServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sam.eurekadiscoveryserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/eurekadiscoveryserverdemo/EurekaDiscoveryServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.eurekadiscoveryserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
